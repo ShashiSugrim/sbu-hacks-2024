5746dc3b127717c25c09b1dcc3d9e3295fab1c3b
Sameer Muzafar <shmeez@Sameers-MacBook-Air.local>
2024-02-10 15:10:45 -0500

    changed name from user to shoe

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -8,27 +8,27 @@ from bson import ObjectId
 user = APIRouter()
 
 @user.get('/')
-async def find_all_users():
+async def find_all_shoes():
     print(conn.local.user.find()) # <pymongo.cursor.Cursor object at 0x102d72d20>  
     print(shoesEntity(conn.local.user.find())) # converts top into good format with ID ... 
     return shoesEntity(conn.local.user.find())
 
 @user.get('/{id}')
-async def find_one_user(id):
+async def find_one_shoe(id):
     return shoeEntity(conn.local.user.find_one({"_id":ObjectId(id)}))
 
 @user.post('/')
-async def create_users(user: Shoes):
+async def create_new_shoe(user: Shoes):
     conn.local.user.insert_one(dict(user))
     return shoesEntity(conn.local.user.find())
 
 @user.put('/{id}')
-async def update_user(id,user: Shoes):
+async def update_shoe(id,user: Shoes):
     (conn.local.user.find_one_and_update({"_id":ObjectId(id)},{
         "$set":dict(user)
     }))
     return shoeEntity(conn.local.user.find_one({"_id":ObjectId(id)}))
 
 @user.delete('/{id}')
-async def delete_user(id,user: Shoes):
+async def delete_shoe(id,user: Shoes):
     return shoeEntity(conn.local.user.find_one_and_delete({"_id":ObjectId(id)}))

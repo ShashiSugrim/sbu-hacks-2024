3a085dd006034dda3c0ab5787a2898efa483b838
Shashi Sugrim <ssugrim1430@bths.edu>
2024-02-11 00:49:08 -0500

    on page 2, fixed positioning of results and filter

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
-// import pagetwocss from "./css/PageTwo.module.css";
+import homecss from "./css/home.module.css";
+
 import SearchBar from "./components/SearchBar";
 import Filter from "./components/Filter";
 import Results from "./components/Results";
 
 function App() {
   return (
-    <div className="App">
+    <div className={homecss.overallStyle}>
       <SearchBar />
-      <div>Search results</div>
-      <div className="content">
-        <Filter className="Filter" />
+      <div style={{ textAlign: "center" }}>Search results</div>
+      <div className={homecss.content}>
+        <Filter className={homecss.Filter} />
         <Results />
       </div>
     </div>
